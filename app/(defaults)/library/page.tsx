3226031ca0fd7f0badbb888ae3507cfd9d303e87
Ican <icandev0220@gmail.com>
2024-11-01 00:40:33 +0900

    ok

--- app/(defaults)/library/page.tsx
+++ app/(defaults)/library/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 import React from 'react';
-import FileExplorerTree from '@/components/library/FileExploreTree';
 import FileExplorer from '@/components/library/FileExplore';
-import { FileNode } from '@/types/File';
 import _files from '@/data/files.json';
-interface LibraryProps {
-  files: FileNode[];
-}
 
-const Library: React.FC<LibraryProps> = ({  }) => {
+const Library = () => {
     const files:any = _files;
   return (
     <div>
