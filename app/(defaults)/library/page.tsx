85ffea902ecfbadd617296abc24b5c120db34b2d
Ican <icandev0220@gmail.com>
2024-11-01 00:44:51 +0900

    login removed

--- app/(defaults)/library/page.tsx
+++ app/(defaults)/library/page.tsx
@@ -4,7 +4,7 @@ import FileExplorer from '@/components/library/FileExplore';
 import _files from '@/data/files.json';
 
 const Library = () => {
-    const files:any = _files;
+  const files:any = _files;
   return (
     <div>
       <FileExplorer initialFiles={files} />
